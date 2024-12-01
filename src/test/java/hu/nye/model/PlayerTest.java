8260598660ccc698898e8cab6f667144cec03157
hr_gergo <156237228+zuhi535@users.noreply.github.com>
2024-12-01 19:05:55 +0100

    Update PlayerTest.java

--- src/test/java/hu/nye/model/PlayerTest.java
+++ src/test/java/hu/nye/model/PlayerTest.java
@@ -10,14 +10,12 @@ public class PlayerTest {
     //ellenőrzi, hogy a Player osztály konstruktora megfelelően inicializálja a játékost a megadott névvel és szimbólummal.
     @Test
     public void testPlayerConstructor_shouldInitializeWithGivenNameAndSymbol() {
-        // Given
+
         String expectedName = "Player1";
         char expectedSymbol = 'Y';
 
-        // When
         Player player = new Player(expectedName, expectedSymbol);
 
-        // Then
         assertEquals(expectedName, player.name());
         assertEquals(expectedSymbol, player.symbol());
     }
@@ -25,15 +23,13 @@ public class PlayerTest {
     //metódusa helyesen adja vissza a játékos string reprezentációját.
     @Test
     public void testToString_shouldReturnCorrectStringRepresentation() {
-        // Given
+        
         String expectedName = "Player1";
         char expectedSymbol = 'X';
         Player player = new Player(expectedName, expectedSymbol);
 
-        // When
         String result = player.toString();
 
-        // Then
         String expectedString = "Player{name='" + expectedName + "', symbol=" + expectedSymbol + "}";
         assertEquals(expectedString, result);
     }
