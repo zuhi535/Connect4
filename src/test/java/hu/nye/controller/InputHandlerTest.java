e99d1074f69e36f02d6d2fb028d0c46ac1bea112
hr_gergo <156237228+zuhi535@users.noreply.github.com>
2024-12-01 19:02:53 +0100

    Update InputHandlerTest.java

--- src/test/java/hu/nye/controller/InputHandlerTest.java
+++ src/test/java/hu/nye/controller/InputHandlerTest.java
@@ -22,71 +22,63 @@ public class InputHandlerTest {
 
     @Test
     public void testGetPlayerMove_validInput() {
-        // Adott
+        
         int validColumn = 3;
-        int columnRange = 6; // Feltételezve, hogy a érvényes oszlop tartomány 0-tól 6-ig terjed
-        when(scannerMock.hasNextInt()).thenReturn(true); // Szimuláljuk, hogy a felhasználó egész számot ad meg
-        when(scannerMock.nextInt()).thenReturn(validColumn); // Szimuláljuk, hogy a felhasználó a 3-as oszlopot adja meg
+        int columnRange = 6;
+        when(scannerMock.hasNextInt()).thenReturn(true); 
+        when(scannerMock.nextInt()).thenReturn(validColumn);
 
-        // Amikor
         int column = inputHandler.getPlayerMove(columnRange);
 
-        // Akkor
         assertEquals(validColumn, column);
     }
 
     @Test
     public void testGetPlayerMove_invalidInput_thenValidInput() {
-        // Adott
+   
         int invalidColumn = -1;
         int validColumn = 2;
-        int columnRange = 5; // Feltételezve, hogy az érvényes oszlop tartomány 0-tól 5-ig terjed
+        int columnRange = 5; 
 
         // Szimuláljuk, hogy először érvénytelen bemenetet ad a felhasználó, majd érvényeset
-        when(scannerMock.hasNextInt()).thenReturn(true, true); // Szimuláljunk két próbálkozást, mindkettő egész szám bemenettel
-        when(scannerMock.nextInt()).thenReturn(invalidColumn, validColumn); // Először érvénytelen, majd érvényes oszlop
+        when(scannerMock.hasNextInt()).thenReturn(true, true); 
+        when(scannerMock.nextInt()).thenReturn(invalidColumn, validColumn); 
 
-        // Amikor
         int column = inputHandler.getPlayerMove(columnRange);
 
-        // Akkor
-        assertEquals(validColumn, column); // Az érvényes oszlopnak kell visszatérnie az érvénytelen bemenet után
+        assertEquals(validColumn, column);
     }
 
     @Test
     public void testGetPlayerMove_inputOutOfRange() {
-        // Adott
+
         int invalidColumn = 7;
         int validColumn = 3;
-        int columnRange = 6; // Feltételezve, hogy az érvényes oszlop tartomány 0-tól 6-ig terjed
+        int columnRange = 6; 
 
         // Szimuláljuk, hogy először érvénytelen, tartományon kívüli bemenetet ad a felhasználó, majd érvényeset
         when(scannerMock.hasNextInt()).thenReturn(true, true);
-        when(scannerMock.nextInt()).thenReturn(invalidColumn, validColumn); // Először érvénytelen, majd érvényes oszlop
+        when(scannerMock.nextInt()).thenReturn(invalidColumn, validColumn); 
 
-        // Amikor
         int column = inputHandler.getPlayerMove(columnRange);
 
-        // Akkor
-        assertEquals(validColumn, column); // Az érvényes oszlopnak kell visszatérnie az érvénytelen tartományon kívüli bemenet után
+        assertEquals(validColumn, column);
     }
 
     @Test
     public void testGetPlayerMove_nonIntegerInput() {
-        // Adott
+        
         String invalidInput = "abc";
         int validColumn = 2;
-        int columnRange = 5; // Érvényes tartomány 0-tól 5-ig
+        int columnRange = 5; 
 
         // Szimuláljuk, hogy nem egész számot ad a felhasználó, majd érvényes egész számot
-        when(scannerMock.hasNextInt()).thenReturn(false, true); // Az első bemenet érvénytelen, a második érvényes
-        when(scannerMock.next()).thenReturn(invalidInput); // Szimuláljuk a nem egész szám bemenetet
-        when(scannerMock.nextInt()).thenReturn(validColumn); // Az érvényes bemenet a második próbálkozásnál
+        when(scannerMock.hasNextInt()).thenReturn(false, true); 
+        when(scannerMock.next()).thenReturn(invalidInput); 
+        when(scannerMock.nextInt()).thenReturn(validColumn); 
 
-        // Amikor
         int column = inputHandler.getPlayerMove(columnRange);
 
-        // Akkor
-        assertEquals(validColumn, column); // Az érvényes oszlopnak kell visszatérnie az érvénytelen bemenet után
+        assertEquals(validColumn, column); 
     }
 }
